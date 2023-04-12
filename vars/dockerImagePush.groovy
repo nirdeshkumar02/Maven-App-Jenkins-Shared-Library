26a608761a9987112609cce12a02935bf034e17b
nirdeshkumar02 <saini.nirdesh.kumar@gmail.com>
2023-04-12 21:49:00 +0530

    changes in dockerImagePush

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,6 +1,6 @@
 def call(String credentialsId, String DockerHubUser, String ImageName, String ImageTag){
-    withCredentials([usernamePassword(credentialsId: ${credentialsId}, passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker login -u ${USER} -p ${PASS}"
+    withCredentials([usernamePassword(credentialsId: 'docker-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "docker login -u '$USER' -p '$PASS' "
     }
     sh """
         docker push ${DockerHubUser}/${ImageName}:${ImageTag}
